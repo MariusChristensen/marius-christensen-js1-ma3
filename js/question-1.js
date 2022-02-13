d4e0a606a85958aa4a28b7681043175718a0da4b
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2022-02-13 15:06:42 +0100

    Finished question 2
    got there in the end!

--- js/question-1.js
+++ js/question-1.js
@@ -1,5 +1,7 @@
+// Answer:
 const getRemainder = (a, b) => a % b;
 
+// Just to check that it works:
 const result = getRemainder(12, 2);
 
 console.log(result);
--- js/question-2.js
+++ js/question-2.js
@@ -1,17 +1,34 @@
+// Answer:
 const GAMES_URL =
-  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=9665856fe1f7473da019ba52cd10b03b";
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=991cdc9aeef144c09cca67cd854041b0";
 
-function getGames() {
-  fetch(GAMES_URL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (results) {
-      console.log(results);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+const gamesContainer = document.querySelector(".games-container");
+
+function createHTML(results) {
+  const gamesList = results;
+
+  gamesContainer.innerHTML = "";
+
+  for (let i = 0; i < gamesList.length; i++) {
+    if (i === 8) {
+      break;
+    }
+    gamesContainer.innerHTML += `<div class="games-container"><ul>
+    <li>Name: ${gamesList[i].name}</li>
+    <li>Rating: ${gamesList[i].rating}</li>
+    <li>Tags: ${gamesList[i].tags.length}</li><ul></div>`;
+  }
+}
+
+async function getGames() {
+  try {
+    const response = await fetch(GAMES_URL);
+    const data = await response.json();
+    const games = data.results;
+    createHTML(games);
+  } catch (error) {
+    gamesContainer.innerHTML = displayError("This error has occurred: " + error);
+  }
 }
 
 getGames();
